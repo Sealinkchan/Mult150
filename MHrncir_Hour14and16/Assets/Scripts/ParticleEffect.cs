511709f79cf52d9c900e0c606f9e4a48d4cc7325
Sealinkchan <sealinkchan@gmail.com>
2025-05-06 16:51:58 -0700

    Hour 14 and 16 Upload
    OOoo...it particles!

---
+++ MHrncir_Hour14and16/Assets/Scripts/ParticleEffect.cs
@@ -0,0 +1,9 @@
+/*
+Changed Start Color to a gradient and the A. 
+Changed Duration to 15
+Changed Gravity modifyer to slant downward
+Went crazy with the start size by making a wave with the Curve
+RoT to 50
+Made the Shape a Circle
+Changed Force Over Lifetime - Set to World and Curve
+*/
---
+++ MHrncir_Hour14and16/Assets/Scripts/UserInterface.cs
@@ -0,0 +1,4 @@
+/*Changed the color of the font for the Title and the Box Narration. I tried to import new fonts. It's too much of a hassle.
+Changed the color for the button in all the different modes it has. 
+Made up a game title (like a Japanese Light Novel title) and a blurb to go along with it.
+Changed the background to a Brandon Sanderson Background from Stormlight Archives*/
